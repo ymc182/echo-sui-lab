afb890af27c9deaf6476da7d5659c1a7cc3e3b73
ymc182 <ymc182@gmail.com>
2022-12-28 14:30:31 +1100

    mock discord

--- src/features/Application/SectionTwo/SectionTwo.tsx
+++ src/features/Application/SectionTwo/SectionTwo.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
 type SectionProps = {
@@ -7,6 +8,24 @@ type SectionProps = {
 	setCurrentSection: React.Dispatch<React.SetStateAction<number>>;
 };
 export default function SectionTwo({ show, id, setCurrentSection }: SectionProps) {
+	//get query from url
+	const [search] = useSearchParams();
+	const [loading, setLoading] = React.useState(false);
+
+	useEffect(() => {
+		//get url without query
+
+		if (search.get("code")) {
+			console.log(search.get("code"));
+			setLoading(true);
+			setTimeout(() => {
+				setLoading(false);
+				toast.success("Verified");
+				setCurrentSection(3);
+			}, 1000);
+		}
+	}, [search]);
+
 	return (
 		<div
 			id={id}
@@ -21,11 +40,15 @@ export default function SectionTwo({ show, id, setCurrentSection }: SectionProps
 
 			<button
 				onClick={() => {
-					toast.info("I will let you go without verifying for now");
-					setCurrentSection((prev) => prev + 1);
+					//redirect to discord oauth
+					//get current url
+					const url = window.location.href.split("?")[0];
+					console.log(url);
+					window.location.href = `https://discord.com/api/oauth2/authorize?client_id=1057494452828127322&redirect_uri=${url}&response_type=code&scope=identify`;
 				}}
+				disabled={loading}
 			>
-				Verify
+				{loading ? "Loading.." : "Verify"}
 			</button>
 		</div>
 	);
